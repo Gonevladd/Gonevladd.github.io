b661790e733d4fd3e75f18bd990778c0c7b3b14b
Vlad <voicehovskiy.vlad@gmail.com>
2022-07-28 01:07:33 +0300

    first success

--- src/main/Search.js
+++ src/main/Search.js
@@ -1,41 +1,89 @@
 import React from "react";
-import {InputGroup, Form, Button} from "react-bootstrap";
+import {Component} from "react";
+import {InputGroup, Form, Button, Image} from "react-bootstrap";
 import myStyle from './style.css'
 import axios from "axios";
 
 
+
+
+
+
+
+
 let searchValue;
 
-function handleSearchInput(event){
-    searchValue = event.target.value;
-}
 
-function handleSearchButton(event){
-    console.log(searchValue);
-    connectToBackend();
-}
-
-function connectToBackend(){
-    axios.post('http://localhost:4000/test', {searchValue})
-        .then(res => console.log(res))
-}
+export default class Search extends Component{
 
 
-export default function Search(){
+    constructor() {
+        super();
+        this.state = {
+            result: "Not yet",
+            imageUrl: "Not yet",
+        };
+        this.handleSearchButton = this.handleSearchButton.bind(this);
+    }
+
+    handleSearchInput(event){
+        searchValue = event.target.value;
+    }
 
 
-    return(
-        <InputGroup className="mb-3" id="search-form">
-            <Form.Control
-                placeholder="Enter movie or series"
-                aria-label="Enter movie or series"
-                aria-describedby="basic-addon2"
-                id="search-input"
-                onChange={handleSearchInput}
-            />
-            <Button variant="outline-success" id="button-addon2" onClick={handleSearchButton}>
-                Find!
-            </Button>
-        </InputGroup>
-    );
+    handleSearchButton(event){
+        console.log(searchValue);
+
+
+        // axios.post('http://localhost:4000/test', {searchValue})
+        //     .then(res => console.log(res))
+
+
+        const options = {
+            method: 'GET',
+            url: 'https://imdb8.p.rapidapi.com/auto-complete',
+            params: {q: searchValue},
+            headers: {
+                'X-RapidAPI-Key': '1fbaf14680msha48a07979f8cfa2p14282ejsn2a81c585dd16',
+                'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+            }
+        };
+
+        axios.request(options).then((response) =>{
+            console.log(response.data);
+
+            console.log(this.state.result);
+
+            this.setState( () => ({result: response.data.d[0].l, imageUrl: response.data.d[0].i.imageUrl}));
+
+        }).catch(function (error) {
+            console.error(error);
+        });
+    }
+
+
+    render() {
+        return(
+            <>
+                <InputGroup className="mb-3" id="search-form">
+                    <Form.Control
+                        placeholder="Enter movie or series"
+                        aria-label="Enter movie or series"
+                        aria-describedby="basic-addon2"
+                        id="search-input"
+                        onChange={this.handleSearchInput}
+                    />
+                    <Button variant="outline-success" id="button-addon2" onClick={this.handleSearchButton}>
+                        Find!
+                    </Button>
+                </InputGroup>
+
+
+                <h1>{this.state.result === "Not yet" ? null : this.state.result}</h1>
+                <Image src={this.state.imageUrl}></Image>
+            </>
+        );
+    }
+
+
 }
