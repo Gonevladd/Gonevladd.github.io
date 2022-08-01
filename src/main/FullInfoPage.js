551a52ef026b627aaed007c035c01e4fed4bf28a
Vlad <voicehovskiy.vlad@gmail.com>
2022-08-01 16:58:01 +0300

    Added some css

--- src/main/FullInfoPage.js
+++ src/main/FullInfoPage.js
@@ -5,6 +5,7 @@ import {Button, Image, Carousel, Card, Row} from "react-bootstrap";
 import Header from "./Header";
 import style from "./style.css"
 import MainColor from "./MainColor";
+import mainColor from "./MainColor";
 
 export default function FullInfoPage(param){
 
@@ -31,13 +32,12 @@ export default function FullInfoPage(param){
 
                     <Row>
                         <Row>
-                            <Card style={{ width: '18rem' }} className="descriptio_item">
+                            <Card style={{ width: '18rem' }} className="description_item">
                                 <Card.Img variant="top" src="https://cdn-icons-png.flaticon.com/512/409/409820.png" />
                                 <Card.Body>
-                                    <Card.Title>Actors</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the
-                                        bulk of the card's content.
+                                    <Card.Title className={"description_item_label " + mainColor.getColor()}>Actors</Card.Title>
+                                    <Card.Text className="description_item_text">
+                                        {from.data.s}
                                     </Card.Text>
                                 </Card.Body>
                             </Card>
@@ -45,13 +45,12 @@ export default function FullInfoPage(param){
 
 
                         <Row>
-                            <Card style={{ width: '18rem' }} className="descriptio_item">
+                            <Card style={{ width: '18rem' }} className="description_item">
                                 <Card.Img variant="top" src="https://static.thenounproject.com/png/2080614-200.png" />
                                 <Card.Body>
-                                    <Card.Title>Year</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the
-                                        bulk of the card's content.
+                                    <Card.Title className={"description_item_label " + mainColor.getColor()}>Year</Card.Title>
+                                    <Card.Text className="description_item_text">
+                                        {from.data.y}
                                     </Card.Text>
                                 </Card.Body>
                             </Card>
@@ -59,13 +58,12 @@ export default function FullInfoPage(param){
 
 
                         <Row>
-                            <Card style={{ width: '18rem' }} className="descriptio_item">
+                            <Card style={{ width: '18rem' }} className="description_item">
                                 <Card.Img variant="top" src="https://cdn-icons-png.flaticon.com/512/2282/2282510.png" />
                                 <Card.Body>
-                                    <Card.Title>Rank</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the
-                                        bulk of the card's content.
+                                    <Card.Title className={"description_item_label " + mainColor.getColor()}>Rank</Card.Title>
+                                    <Card.Text className="description_item_text">
+                                        #{from.data.rank}
                                     </Card.Text>
                                 </Card.Body>
                             </Card>
@@ -73,13 +71,12 @@ export default function FullInfoPage(param){
 
 
                         <Row>
-                            <Card style={{ width: '18rem' }} className="descriptio_item">
+                            <Card style={{ width: '18rem' }} className="description_item">
                                 <Card.Img variant="top" src="https://i.pinimg.com/originals/aa/f7/05/aaf705e06726ce3881288ae4be3ac5fe.jpg" />
                                 <Card.Body>
-                                    <Card.Title>Type</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the
-                                        bulk of the card's content.
+                                    <Card.Title className={"description_item_label " + mainColor.getColor()}>Type</Card.Title>
+                                    <Card.Text className="description_item_text">
+                                        {from.data.q}
                                     </Card.Text>
                                 </Card.Body>
                             </Card>
@@ -87,7 +84,7 @@ export default function FullInfoPage(param){
 
                     </Row>
 
-
+                    {from.data.v !== undefined ?
                     <Carousel activeIndex={index} onSelect={handleSelect} className="carousel_item">
                         <Carousel.Item>
                             <img
@@ -111,6 +108,7 @@ export default function FullInfoPage(param){
                             />
                         </Carousel.Item>
                     </Carousel>
+                        : null}
                 </div>
 
 
--- src/main/Header.js
+++ src/main/Header.js
@@ -6,18 +6,68 @@ import {Nav} from "react-bootstrap";
 import {Navbar} from "react-bootstrap";
 import {NavDropdown} from "react-bootstrap";
 import MainColor from "./MainColor";
+import mainColor from "./MainColor";
+import Search from "./Search";
+import axios from "axios";
+import PreviouslySearch from "./PreviouslySearch";
 
 
 export default function Header(){
+
+    let searchValue;
 
     let chooseColor = event => {
         MainColor.setColor(event.target.innerText);
     }
 
+    let handleSearchInput = event =>{
+        searchValue = event.target.value;
+    }
+
+    let handleSearchButton = event => {
+        console.log(searchValue);
+
+        // this.setState(() => ({showLoading: "Yes"}));
+
+        // if(this.checkPrevious){
+        //     searchValue = PreviouslySearch.getSearchResult().q;
+        // PreviouslySearch.setSearchResult(null);
+        // document.getElementById('button-addon2').click();
+        // }
+
+
+        // axios.post('http://localhost:4000/test', {searchValue})
+        //     .then(res => console.log(res))
+
+
+
+        const options = {
+            method: 'GET',
+            url: 'https://imdb8.p.rapidapi.com/auto-complete',
+            params: {q: searchValue},
+            headers: {
+                'X-RapidAPI-Key': '1fbaf14680msha48a07979f8cfa2p14282ejsn2a81c585dd16',
+                'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+            }
+        };
+
+        axios.request(options).then((response) => {
+
+            console.log(response.data);
+            if(response.data.symbol === null){
+                console.log('Bad data');
+                return;
+            }
+
+        }).catch(function (error) {
+            console.error(error);
+        });
+    }
+
     return(
             <Navbar bg="light" expand="lg">
                 <Container fluid>
-                    <Navbar.Brand href="/">Film rates</Navbar.Brand>
+                    <Navbar.Brand href="/"><div className={mainColor.getColor()}>MOVIE RATE</div></Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                         <Nav
@@ -45,8 +95,9 @@ export default function Header(){
                             placeholder="Search"
                             className="me-2"
                             aria-label="Search"
+                            onChange={handleSearchInput}
                         />
-                        <Button variant={'outline-'+MainColor.getColor()}>Find!</Button>
+                        <Button variant={'outline-'+MainColor.getColor()} onClick={handleSearchButton}>Find!</Button>
                     </Form>
                 </Container>
             </Navbar>
